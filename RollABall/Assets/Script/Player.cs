ab22a60998dc4563c696d5802fcdebebdc308e8e
chen199209 <610115623@qq.com>
2018-03-08 17:42:32 +0800

    base

---
+++ RollABall/Assets/Script/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+
+	private Rigidbody rigidbody;
+
+	public float speed = 3.0f;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float h = Input.GetAxis("Horizontal");
+		float v = Input.GetAxis("Vertical"); 
+		rigidbody.AddForce(new Vector3(h*speed,0,v*speed));
+	}
+}
